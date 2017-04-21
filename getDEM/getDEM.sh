e306645629d5ba114de523413a30ff3674be1f4a
joel <joelfiddes@gmail.com>
2017-04-21 11:25:12 +0200

    getDEM.sh work

--- getDEM/getDEM.sh
+++ getDEM/getDEM.sh
@@ -5,24 +5,25 @@
 #erathdata login: https://urs.earthdata.nasa.gov/profile
 #gdal_translate
 
-eio clip -o yala.tif --bounds 12.35 41.8 12.65 42
-eio clip -o test1.tif --bounds 12.35 31.8 12.65 32.1
-
-eio clip -o langtang.tif --bounds 85.52 28.1 85.68 28.3
-85.52 28.1 85.68 28.2
+#pypi/elevation
+# eio clip -o yala.tif --bounds 12.35 41.8 12.65 42
+# eio clip -o test1.tif --bounds 12.35 31.8 12.65 32.1
+# eio clip -o langtang.tif --bounds 85.52 28.1 85.68 28.3
+# 85.52 28.1 85.68 28.2
 
 
 #this works
-wget --user USER --password PWD http://e4ftl01.cr.usgs.gov//MODV6_Dal_D/SRTM/SRTMGL1.003/2000.02.11/N28E085.SRTMGL1.hgt.zip
-unzip  N28E085.SRTMGL1.hgt.zip
-gdal_translate -q -co TILED=YES -co COMPRESS=DEFLATE -co ZLEVEL=9 -co PREDICTOR=2 N28E085.hgt SRTMDAT.tif
 
+# wget --user USER --password PWD http://e4ftl01.cr.usgs.gov//MODV6_Dal_D/SRTM/SRTMGL1.003/2000.02.11/N28E085.SRTMGL1.hgt.zip
+# unzip  N28E085.SRTMGL1.hgt.zip
+# gdal_translate -q -co TILED=YES -co COMPRESS=DEFLATE -co ZLEVEL=9 -co PREDICTOR=2 N28E085.hgt SRTMDAT.tif
+
+#naming convention  http://e4ftl01.cr.usgs.gov//MODV6_Dal_D/SRTM/SRTMGL1.003/2000.02.11/
 #name of dem file is llcorner 1deg tiles
 # to find tile need to round down long/let to nearest degree
 
 
 #parse credentials file to get user/pwd
-read.Files('~/.credentials_earthdata')
 USER=unlist(strsplit(readLines('~/.credentials_earthdata')[[1]],'='))[2]
 PWD=unlist(strsplit(readLines('~/.credentials_earthdata')[[2]],'='))[2]
 #==================================================================
@@ -37,35 +38,93 @@ df=data.frame(lon,lat)
 #find unique llcorner
 df2=unique(floor(df))
 
-# construct query
-#filename str = N28E08
-
+#clean up
+system("rm SRTMDAT*")
+system("rm *.hgt")
 
 for (i in 1:(dim(df2)[1])){
+	if (sign(df2$lat[i])==-1){LATVAL<-'S'}
+	if (sign(df2$lat[i])==1){LATVAL<-'N'}
+	if (sign(df2$lon[i])==-1){LONVAL<-'W'}
+	if (sign(df2$lon[i])==1){LONVAL<-'E'}
+	lon_pretty=formatC(df2$lon[i],width=3,flag='0')
+	#get tile
+	filetoget=paste0(LATVAL,df2$lat[i],LONVAL,lon_pretty,'.SRTMGL1.hgt.zip')
+	filetogetUNZIP=paste0(LATVAL,df2$lat[i],LONVAL,lon_pretty,'.hgt')
 
-if (sign(df2$lat[i])==-1){LATVAL<-'S'}
-if (sign(df2$lat[i])==1){LATVAL<-'N'}
-if (sign(df2$lon[i])==-1){LONVAL<-'W'}
-if (sign(df2$lon[i])==1){LONVAL<-'E'}
-
-lon_pretty=formatC(df2$lon[i],width=3,flag='0')
-
-#get tile
-system(paste0('wget --user ', USER ,  ' --password ' ,PWD, ' http://e4ftl01.cr.usgs.gov//MODV6_Dal_D/SRTM/SRTMGL1.003/2000.02.11/',LATVAL,df2$lat[i],LONVAL,lon_pretty,'.SRTMGL1.hgt.zip'))
+if (file.exists(filetoget)){ #dont download again
+   print(paste0(filetoget, " exists"))
+   	system(paste0("unzip ", filetoget))
+	system(paste0("gdal_translate -q -co TILED=YES -co COMPRESS=DEFLATE -co ZLEVEL=9 -co PREDICTOR=2 ", filetogetUNZIP, " SRTMDAT",i,".tif"))
+} else {
+ 
+	system(paste0('wget --user ', USER ,  ' --password ' ,PWD, ' http://e4ftl01.cr.usgs.gov//MODV6_Dal_D/SRTM/SRTMGL1.003/2000.02.11/',filetoget))
+	# extract
+	system(paste0("unzip ", filetoget))
+	system(paste0("gdal_translate -q -co TILED=YES -co COMPRESS=DEFLATE -co ZLEVEL=9 -co PREDICTOR=2 ", filetogetUNZIP, " SRTMDAT",i,".tif"))
+}
 }
 
 #
-gdal_translate -q -co TILED=YES -co COMPRESS=DEFLATE -co ZLEVEL=9 -co PREDICTOR=2 N28E085.hgt SRTMDAT.tif
+
 
 #==================================================================
 # DEM retrieval based on bbox
 #=================================================================
 #bbox=(e,s,w,n)
-bbox=c(85.52, 27.8, 89.68, 29.3)
+bbox=c(85.1, 27.8, 86.8, 28.7)
 floorbox=floor(bbox)
 lonseq=seq(floorbox[1],floorbox[3],1)
 latseq=seq(floorbox[2],floorbox[4],1)
 gridstoget=expand.grid(lonseq,latseq)
+names(gridstoget)<-c('lon', 'lat')
+df2<-gridstoget
+#cleanup
+system("rm SRTMDAT*")
+system("rm *.hgt")
+
+for (i in 1:(dim(df2)[1])){
+	if (sign(df2$lat[i])==-1){LATVAL<-'S'}
+	if (sign(df2$lat[i])==1){LATVAL<-'N'}
+	if (sign(df2$lon[i])==-1){LONVAL<-'W'}
+	if (sign(df2$lon[i])==1){LONVAL<-'E'}
+	lon_pretty=formatC(df2$lon[i],width=3,flag='0')
+	#get tile
+	filetoget=paste0(LATVAL,df2$lat[i],LONVAL,lon_pretty,'.SRTMGL1.hgt.zip')
+	filetogetUNZIP=paste0(LATVAL,df2$lat[i],LONVAL,lon_pretty,'.hgt')
+
+if (file.exists(filetoget)){ #dont download again
+   print(paste0(filetoget, " exists"))
+   	system(paste0("unzip ", filetoget))
+	system(paste0("gdal_translate -q -co TILED=YES -co COMPRESS=DEFLATE -co ZLEVEL=9 -co PREDICTOR=2 ", filetogetUNZIP, " SRTMDAT",i,".tif"))
+} else {
+ 
+	system(paste0('wget --user ', USER ,  ' --password ' ,PWD, ' http://e4ftl01.cr.usgs.gov//MODV6_Dal_D/SRTM/SRTMGL1.003/2000.02.11/',filetoget))
+	# extract
+	system(paste0("unzip ", filetoget))
+	system(paste0("gdal_translate -q -co TILED=YES -co COMPRESS=DEFLATE -co ZLEVEL=9 -co PREDICTOR=2 ", filetogetUNZIP, " SRTMDAT",i,".tif"))
+}
+}
+
+#==================================================================
+# MERGE RASTER
+#=================================================================
+demfiles=list.files(pattern="SRTMDAT*")
+
+rasters1 <- list.files(pattern="SRTMDAT*",full.names=TRUE, recursive=FALSE)
+rast.list <- list()
+  for(i in 1:length(rasters1)) { rast.list[i] <- raster(rasters1[i]) }
+
+# And then use do.call on the list of raster objects
+rast.list$fun <- mean
+  rast.mosaic <- do.call(mosaic,rast.list)
+    plot(rast.mosaic)
+
+#==================================================================
+# EXTRACT TOPO
+#================================================================= 
+
+
 
 #download
 #asp/slp - R raster
@@ -76,9 +135,6 @@ gridstoget=expand.grid(lonseq,latseq)
 require(horizon)
   r <- getData('alt', country='CH')
      s <- svf(r, nAngles=8, maxDist=500, ll=TRUE)
-
-
-
 
 #identify tiles required for given point or poinst or area
 
