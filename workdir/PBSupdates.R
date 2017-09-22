75217308c28a615f38e974d821724ee9e47e0b22
joel <joelfiddes@gmail.com>
2017-09-22 10:11:59 +0200

    remove args #config['geotop']['beg'] config['geotop']['end'] this to accept main time parameters

--- workdir/PBSupdates.R
+++ workdir/PBSupdates.R
@@ -5,7 +5,7 @@
 assign weights bigPix to landform resolution, dissagrgate (rasterstack = smlPix * w)
 
 clusterloop i in 1:50:
-	extract vectors for each pixel in cluster[i] and average = size 1*100 # https://stackoverflow.com/questions/19833784/how-to-extract-values-from-rasterstack-with-xy-coordinates
+	extract vectors for each pixel in cluster[i] and average = size 1*100 # https://stackoverflow.com/questions/19833784/how-to-extract-values-from-rasterstack-with-coordinates-xy
 	end
 matrix = 100 * 50
 
